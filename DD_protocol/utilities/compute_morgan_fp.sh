0dad3db34953e62eb6cc01c2996304bc95a869ca
Rasmus Hildebrandt <98971156+RasmusHildebrandt@users.noreply.github.com>
2022-07-01 03:25:45 +0100

    Update compute_morgan_fp.sh

--- DD_protocol/utilities/compute_morgan_fp.sh
+++ DD_protocol/utilities/compute_morgan_fp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=12:00:00
-#PBS -l select=1:ncpus=32:mem=32gb
+
+total_processors =
 
 
 cd $PBS_O_WORKDIR
@@ -11,7 +12,7 @@ conda activate dd-env
 
 start=`date +%s`
 
-python -u DD_protocol/utilities/morgan_fp.py -sfp library_prepared -fn library_prepared_fp -tp 32
+python -u DD_protocol/utilities/morgan_fp.py -sfp library_prepared -fn library_prepared_fp -tp $total_processors
 
 end=`date +%s`
 runtime=$((end-start))
