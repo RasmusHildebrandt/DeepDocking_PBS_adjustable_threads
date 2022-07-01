f2ff91fb1f69c7cb2d8bd17811e9182549147094
Rasmus Hildebrandt <98971156+RasmusHildebrandt@users.noreply.github.com>
2022-07-01 03:21:10 +0100

    Update phase_1.sh

--- DD_protocol/phase_1.sh
+++ DD_protocol/phase_1.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 #PBS -l walltime=12:00:00
+
 #PBS -N phase_1
 
-cd $PBS_O_WORKDIR
+cd $file_path
 
 module load anaconda3/personal
 
 source ~/.bashrc
-conda activate $environment
+conda activate dd-env
 
 start=`date +%s`
 
-file_path=`sed -n '1p' $project_dir/$project_name/logs.txt`
-protein=`sed -n '2p' $project_dir/$project_name/logs.txt`
-n_mol=`sed -n '8p' $project_dir/$project_name/logs.txt`
+file_path=`sed -n '1p' $file_path/$project_name/logs.txt`
+protein=`sed -n '2p' $file_path/$project_name/logs.txt`
+n_mol=`sed -n '8p' $file_path/$project_name/logs.txt`
+max_jobs=`sed -n '11p' $file_path/projects/project_name/logs.txt`
+max_threading=`sed -n '10p' $file_path/projects/project_name/logs.txt`
 
 pr_it=$(($iteration-1)) 
 
@@ -28,8 +31,6 @@ else
     	to_d=$mol_to_dock
 fi
 
-echo $to_d
-echo $t_cpu
 
 python DD_protocol/jobid_writer.py -pt $protein -fp $file_path -n_it $iteration -jid $PBS_JOBNAME -jn $PBS_JOBNAME.txt
 
@@ -39,11 +40,11 @@ sdf_directory=`sed -n '6p' $project_dir/$project_name/logs.txt`
 
 if [ $iteration == 1 ];then pred_directory=$morgan_directory;else pred_directory=$file_path/$protein/iteration_$pr_it/morgan_1024_predictions;fi
 
-python DD_protocol/scripts_1/molecular_file_count_updated.py -pt $protein -it $iteration -cdd $pred_directory -t_pos $t_cpu -t_samp $to_d
-python DD_protocol/scripts_1/sampling.py -pt $protein -fp $file_path -it $iteration -dd $pred_directory -t_pos $t_cpu -tr_sz $mol_to_dock -vl_sz $n_mol
+python DD_protocol/scripts_1/molecular_file_count_updated.py -pt $protein -it $iteration -cdd $pred_directory -t_pos $max_threading -t_samp $to_d
+python DD_protocol/scripts_1/sampling.py -pt $protein -fp $file_path -it $iteration -dd $pred_directory -t_pos $max_threading -tr_sz $mol_to_dock -vl_sz $n_mol
 python DD_protocol/scripts_1/sanity_check.py -pt $protein -fp $file_path -it $iteration
-python DD_protocol/scripts_1/extracting_morgan.py -pt $protein -fp $file_path -it $iteration -md $morgan_directory -t_pos $t_cpu
-python DD_protocol/scripts_1/extracting_smiles.py -pt $protein -fp $file_path -it $iteration -smd $smile_directory -t_pos $t_cpu
+python DD_protocol/scripts_1/extracting_morgan.py -pt $protein -fp $file_path -it $iteration -md $morgan_directory -t_pos $max_threading
+python DD_protocol/scripts_1/extracting_smiles.py -pt $protein -fp $file_path -it $iteration -smd $smile_directory -t_pos $max_threading
 
 end=`date +%s`
 runtime=$((end-start))
