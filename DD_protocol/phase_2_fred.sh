bd0650c42db5011e27bd264b307fc1afb8afb294
Rasmus Hildebrandt <98971156+RasmusHildebrandt@users.noreply.github.com>
2022-07-01 03:21:39 +0100

    Update phase_2_fred.sh

--- DD_protocol/phase_2_fred.sh
+++ DD_protocol/phase_2_fred.sh
@@ -3,7 +3,7 @@
 #PBS -l select=1:ncpus=1:mem=1gb
 #PBS -N phase_2
 
-cd $PBS_O_WORKDIR
+cd $file_path
 source ~/.profile
 module load anaconda3/personal
 
@@ -12,12 +12,11 @@ c_mem=2
 unit="gb"
 t_mem="$((t_node * c_mem))$unit"
 
-file_path=`sed -n '1p' $project_dir/$project_name/logs.txt`
-protein=`sed -n '2p' $project_dir/$project_name/logs.txt`
+file_path=`sed -n '1p' $file_path/projects/$project_name/logs.txt`
+protein=`sed -n '2p' $file_path/projects/$project_name/logs.txt`
 
-
-morgan_directory=`sed -n '4p' $project_dir/$project_name/logs.txt`
-smile_directory=`sed -n '5p' $project_dir/$project_name/logs.txt`
+morgan_directory=`sed -n '4p' $file_path/projects/$project_name/logs.txt`
+smile_directory=`sed -n '5p' $file_path/projects/$project_name/logs.txt`
 
 
 python DD_protocol/jobid_writer.py -pt $protein -fp $file_path -n_it $iteration -jid $PBS_JOBNAME -jn $PBS_JOBNAME.txt
@@ -33,14 +32,14 @@ do
    if [ $tmp = test ];then name=testing;fi
    echo "#!/bin/bash
 #PBS -l walltime=12:00:00
-#PBS -J 0-255
+
 cd $PBS_O_WORKDIR
 echo \$f_1
 echo \$name1
 echo \$t_nod1
-oeomega pose -in $file_path/$protein/iteration_$iteration/smile/\$tmp1\_smiles_final_updated/\$tmp1\_smiles_final_updated_\$PBS_ARRAY_INDEX.smi -out $file_path/$protein/iteration_$iteration/sdf/\$name1\_\$PBS_ARRAY_INDEX\_sdf.oeb.gz -strictstereo false -log \$name1.log -prefix \$name1\_\$PBS_ARRAY_INDEX">> $name'_'conf.sh
+oeomega pose -in $file_path/$protein/iteration_$iteration/smile/\$tmp1\_smiles_final_updated/\$tmp1\_smiles_final_updated_\$PBS_ARRAY_INDEX.smi -out $file_path/$protein/iteration_$iteration/sdf/\$name1\_ame1\_\$PBS_ARRAY_INDEX\_sdf.oeb.gz -strictstereo false -log \$name1.log -prefix \$name1\_\$PBS_ARRAY_INDEX">> $name'_'conf.sh
 
-   qsub -N $PBS_JOBNAME -l select=1:ncpus=$t_node:mem=$t_mem:cpu_type=ivy -v f_1="$f",name1="$name",t_nod1="$t_nod",tmp1="$tmp" $name'_'conf.sh
+   qsub -N $PBS_JOBNAME -l select=1:ncpus=$t_node:mem=$t_mem -v f_1="$f",name1="$name",t_nod1="$t_nod",tmp1="$tmp" $name'_'conf.sh
 done
 wait
 
