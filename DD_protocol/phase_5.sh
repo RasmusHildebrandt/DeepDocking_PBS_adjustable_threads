af0671a90dff196e2c2d38eb5a1f255e378716a5
Rasmus Hildebrandt <98971156+RasmusHildebrandt@users.noreply.github.com>
2022-07-01 03:24:20 +0100

    Update phase_5.sh

--- DD_protocol/phase_5.sh
+++ DD_protocol/phase_5.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 #PBS -l walltime=12:00:00
-#PBS -l select=1:ncpus=1:mem=12gb:ngpus=1
 #PBS -N phase_5
 
-cd $PBS_O_WORKDIR
+cd $file_path
 module load anaconda3/personal
 
 
-env=${en}
+env=dd-env
 
 source ~/.bashrc
 conda activate $env
 
-file_path=`sed -n '1p' $eval_l/$project_n/logs.txt`
-protein=`sed -n '2p' $eval_l/$project_n/logs.txt`    # name of project folder
+file_path=`sed -n '1p' $file_path/projects/$project_name/logs.txt`
+protein=`sed -n '2p' $file_path/projects/$project_name/logs.txt`    # name of project folder
 
-morgan_directory=`sed -n '4p' $eval_l/$project_n/logs.txt`
+morgan_directory=`sed -n '4p' $file_path/projects/$project_name/logs.txt`
 
-num_molec=`sed -n '8p' $eval_l/$project_n/logs.txt`
+num_molec=`sed -n '8p' $file_path/projects/$project_name/logs.txt`
 
 gpu_part=gp_p
 
@@ -31,3 +30,4 @@ python DD_protocol/scripts_2/simple_job_predictions.py -pt $protein -fp $file_pa
 cd $file_path/$protein/iteration_$iteration/simple_job_predictions/
 echo "running simple_jobs"
 for f in *.sh;do qsub $f;done
+
